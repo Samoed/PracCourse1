081fe813ae0d554e5e44bafdb32b79c014c9ca2b
Samoed <samoed.roman@gmail.com>
2020-04-01 21:39:30 +0500

    task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Task1
+{
+  internal class Program
+  {
+      static bool Chek(int q,int x, int y, int[,] arr)
+      {
+          bool fl;
+          if (q == 1)
+          {
+             return arr[x + 1, y] == 0 && arr[x, y + 1] == 0 && arr[x+1, y+1] == 0;
+          }
+          if (q == 2)
+          {
+              return arr[x, y] == 0 && arr[x + 1, y] == 0 && arr[x+1, y+1] == 0;
+          }
+          if (q == 3)
+          {
+              return arr[x, y] == 0 && arr[x, y + 1] == 0 && arr[x+1, y+1] == 0;
+          }
+          return arr[x, y] == 0 && arr[x + 1, y] == 0 && arr[x, y + 1] == 0;
+          
+      }
+    public static void Main(string[] args)
+    {
+        string[] str=Console.ReadLine().Split(' ');
+        int n=int.Parse(str[0]);
+        int m=int.Parse(str[1]);
+        int[,] arr = new int[n,m];
+        int q = int.Parse(Console.ReadLine());
+        for (int i = 0; i < q; ++i)
+        {
+            str=(Console.ReadLine().Split(' '));
+            int t = int.Parse(str[0]);
+            int x = int.Parse(str[1]);
+            int y = int.Parse(str[2]);
+            x--;y--;
+            //Console.WriteLine($"x={x} y={y} arr={arr[x,y]}");
+            switch (t)
+            {
+                case 1:
+                    if (x < n-1 && y < m-1 && x>=0 && y>=0)
+                    {
+                        //Console.WriteLine("ok");
+                        if (Chek(t,x,y,arr)){
+                            arr[x+1, y] = 1;
+                            arr[x, y+1] = 1;
+                            arr[x+1, y+1] = 1;
+                        }
+                    }
+                    break;
+                case 2:
+                    if (x < n-1 && y < m-1 && x>=0 && y>=0)
+                    {
+                        if (Chek(t,x, y, arr))
+                        {
+                          //  Console.WriteLine("ok");
+                            arr[x, y] = 1;
+                            arr[x + 1, y] = 1;
+                            arr[x + 1, y + 1] = 1;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (x < n-1 && y < m-1 && x>=0 && y>=0)
+                    {
+                        if (Chek(t,x, y, arr))
+                        {
+                            //Console.WriteLine("ok");
+                            arr[x, y] = 1;
+                            arr[x, y + 1] = 1;
+                            arr[x + 1, y + 1] = 1;
+                        }
+                    }
+                    break;
+                case 4:
+                    if (x < n-1 && y < m-1 && x>=0 && y>=0)
+                    {
+                        if (Chek(t,x, y, arr))
+                        {
+                            //Console.WriteLine("ok");
+                            arr[x, y] = 1;
+                            arr[x + 1, y] = 1;
+                            arr[x, y + 1] = 1;
+                        }
+                    }
+                    break;
+            }
+            /*for (int w = 0; w < n; ++w)
+            {
+                for (int j = 0; j < m; ++j)
+                    Console.Write(arr[w,j]+" ");
+                Console.WriteLine();
+            }*/
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                ans += arr[i, j];
+            }
+        }
+        Console.WriteLine(ans);
+    }
+  }
+}

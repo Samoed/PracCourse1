bf999cd1dbe104a2515eed4d887626a4b2738555
Samoed <samoed.roman@gmail.com>
2020-04-02 12:56:19 +0500

    Task3

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Task3
+﻿using System;
+
+namespace Task3
 {
     internal class Program
     {
+     public static int ReadInt(int left = -100, int right = 100)
+            {
+                bool ok = false;
+                int number = 0;
+                do
+                {
+                    try
+                    {
+                        number = int.Parse(Console.ReadLine());
+                        if (number >= left && number <= right) ok = true;
+                        else
+                        {
+                            Console.WriteLine($"Ошибка. Число выход за границы. Введите число большее {left} и меньшее {right}");
+                            ok = false;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка. Введено не целое число. Введите целое число.");
+                        ok = false;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"Ошибка. Число выход за границы. Введите число большее {left} и меньшее {right}");
+                        ok = false;
+                    }
+                } while (!ok);
+                return number;
+            }
         public static void Main(string[] args)
         {
-            int x 
+            Console.WriteLine("Введите число x");
+            int x = ReadInt();
+            Console.WriteLine("Введите число y");
+            int y = ReadInt(), u = x;
+            if (x*x+y*y<=4 && x*x+y*y>=1 && y>=0)
+                u=0;
+            Console.WriteLine(u);
         }
     }
 }

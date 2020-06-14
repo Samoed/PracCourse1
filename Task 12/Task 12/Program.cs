563d085b93447595b337944f64d3aa25c145ff9c
Samoed <samoed.roman@gmail.com>
2020-06-14 17:04:32 +0500

    report

--- Task 12/Task 12/Program.cs
+++ Task 12/Task 12/Program.cs
@@ -17,8 +17,8 @@ public class Programm
     static void MakeHeap(int[] arr, int n, int i)
     {
         int largest = i;
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
         if (l < n && arr[l] > arr[largest])
             largest = l;
         if (r < n && arr[r] > arr[largest])
